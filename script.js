674fb817a9580a1f8f14f1306e521104b4b9f65c
Saeedafshari11235 <msafshari11235@gmail.com>
2022-08-05 19:26:51 +0430

    adding validation

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+let strength
+let validation =[]
 let password = document.querySelector("#passinp")
-let submit = document.querySelector("#submit")
-submit.addEventListener("click",function(){
-    console.log(password)
-})
+validation=[(password.length>5),
+    (password.search(/[A,Z]/)>-1),
+    (password.search(/[0,9]/)>-1),
+    (password.search(/[!@#$%^&*_?-;:.,]/)>-1)
+]
