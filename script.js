a503a23c95560e01ecdcef509f5b3e0acfa38b98
Saeedafshari11235 <msafshari11235@gmail.com>
2022-08-05 19:48:45 +0430

    setting validation function and styles

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-let strength
+let strength,password
 let validation =[]
-let password = document.querySelector("#passinp")
-validation=[(password.length>5),
-    (password.search(/[A,Z]/)>-1),
-    (password.search(/[0,9]/)>-1),
-    (password.search(/[!@#$%^&*_?-;:.,]/)>-1)
-]
+let passwordtag = document.querySelector("#passinp")
+let red = document.querySelector(".red")
+let yellow = document.querySelector(".yellow")
+let green = document.querySelector(".green")
+function valid(){
+    password=passwordtag.value
+    validation=[(password.length>5),
+        (password.search(/[A,Z]/)>-1),
+        (password.search(/[0,9]/)>-1),
+        (password.search(/[!@#.?-]/)>-1)
+    ]
+    strength = validation.filter(function(x){return x==1}).length
+    if(strength==1){
+        red.style.visibility="visible"
+    }
+    if(strength==2){
+        yellow.style.visibility="visible"
+    }
+    if(strength==3){
+        green.style.visibility="visible"
+    }
+}
