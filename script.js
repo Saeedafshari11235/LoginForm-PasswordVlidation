7839fe6df5d7d5281b5fd5039533cdffe3c3920c
Saeedafshari11235 <msafshari11235@gmail.com>
2022-08-06 23:21:05 +0430

    setting password status

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let red = document.querySelector(".red")
 let orange = document.querySelector(".orange")
 let yellow = document.querySelector(".yellow")
 let green = document.querySelector(".green")
+let stts = document.querySelector("#statusTxt")
 function valid(){
     password=passwordtag.value
     validation=[
@@ -13,20 +14,27 @@ function valid(){
         (password.search(/[!@#.?-]/)>-1)
     ]
     strength = validation.filter(function(x){return x==1}).length
+    console.log(strength)
     if(password.length>=8){
         red.style.visibility="visible"
+        stts.innerHTML="Very weak"
         if(strength>=1){
             orange.style.visibility="visible"
+            stts.innerHTML="Weak"
         }else{
             orange.style.visibility="hidden"
+
         }
         if(strength>=2){
             yellow.style.visibility="visible"
+            stts.innerHTML="Strong"
         }else{
             yellow.style.visibility="hidden"
+
         }
         if(strength==3){
             green.style.visibility="visible"
+            stts.innerHTML="Very strong"
         }else{
             green.style.visibility="hidden"
         }
@@ -35,5 +43,6 @@ function valid(){
         orange.style.visibility="hidden"
         yellow.style.visibility="hidden"
         green.style.visibility="hidden"
+        stts.innerHTML=""
     }
 }
